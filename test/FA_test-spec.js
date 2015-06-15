1e5280ffff6149364c66a337bcee87bab1f12126
Constanti <constantin.dumba@gmail.com>
2015-06-15 10:27:37 -1000

    changed name

--- test/FA_test-spec.js
+++ test/FA_test-spec.js
@@ -2,30 +2,30 @@ var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
-var FA = require('../FA.js');
+var factorialAlgorithm = require('../FA.js');
 
 describe('Factorial Algorithm', function () {
 
   it('should be a function', function () {
-    FA.factorial.should.be.a("function");
+    factorialAlgorithm.factorial.should.be.a("function");
   });
 
   it('should take a number as an argument and return a number', function () {
-    expect(function(){FA.factorial('a')}).to.throw('you need to give me a number mate');
-    FA.factorial(4).should.be.a("number");
+    expect(function(){factorialAlgorithm.factorial('a')}).to.throw('you need to give me a number mate');
+    factorialAlgorithm.factorial(4).should.be.a("number");
 
   });
 
   it('should return the factorial of an input number', function () {
-    FA.factorial(5).should.equal(120);
-    FA.factorial(6).should.equal(720);
-    FA.factorial(1).should.equal(1);
-    FA.factorial(2).should.equal(2);
+    factorialAlgorithm.factorial(5).should.equal(120);
+    factorialAlgorithm.factorial(6).should.equal(720);
+    factorialAlgorithm.factorial(1).should.equal(1);
+    factorialAlgorithm.factorial(2).should.equal(2);
   });
 
   it('should return zero if given a negative number', function () {
-    FA.factorial(-1).should.equal(0);
-    FA.factorial(-5).should.equal(0);
+    factorialAlgorithm.factorial(-1).should.equal(0);
+    factorialAlgorithm.factorial(-5).should.equal(0);
   });
 
 });
