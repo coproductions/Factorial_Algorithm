f593e7535ab1f40fd504ed941a3c056de20be8b9
Constanti <constantin.dumba@gmail.com>
2015-06-15 11:17:24 -1000

    formatting improvement

--- FA.js
+++ FA.js
@@ -2,15 +2,16 @@ function factorial(x){
   if(typeof x !== 'number'){
     throw new Error('you need to give me a number mate')
   }
+
   if(x <= 0){
     return 0;
   }
 
-    if(x===1){
-      return x;
-      }
+  if(x===1){
+    return x;
+    }
 
-    return x * factorial(x-1);
+  return x * factorial(x-1);
 };
 
 module.exports = {
